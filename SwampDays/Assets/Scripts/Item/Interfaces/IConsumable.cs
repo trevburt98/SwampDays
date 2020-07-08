7f924695fb8913fd28dbe8faff28dc5d7b170448
wpendleton <wnpendleton@gmail.com>
2020-07-08 17:33:08 -0500

    Refactored directory structure to fix file tracking issues

--- SwampDays/Assets/Scripts/Item/Interfaces/IConsumable.cs
+++
@@ -1,9 +0,0 @@
-﻿using Character.PlayerCharacter;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IConsumable : IInteractable
-{
-    void use(ICharacter<float> user);
-}
