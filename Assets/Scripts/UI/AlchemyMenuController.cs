3cc6e125d7cdfa3d13debd257982ce9655923267
wpendleton <wnpendleton@gmail.com>
2020-07-31 16:27:29 -0500

    Implemented heat panel

--- Assets/Scripts/UI/AlchemyMenuController.cs
+++ Assets/Scripts/UI/AlchemyMenuController.cs
@@ -24,16 +24,22 @@ public class AlchemyMenuController : MonoBehaviour
     private Button currentButton;
     List<GameObject> currentButtons;
 
+    [SerializeField] private Slider heatSlider;
+    [SerializeField] private InputField heatInputField;
+
+    private bool valueUpdated = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        heatSlider.onValueChanged.AddListener(delegate { updateHeat((int)heatSlider.value); });
+        heatInputField.onValueChanged.AddListener(delegate { updateHeat(heatInputField.text); });
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        valueUpdated = false;
     }
 
     public void ToggleAlchemyMenu(bool active, List<AlchemyBase> baseList, int numIngredients)
@@ -41,6 +47,7 @@ public class AlchemyMenuController : MonoBehaviour
         try
         {
             gameObject.SetActive(active);
+            clearPanels();
             foreach (AlchemyBase alchemyBase in baseList)
             {
                 GameObject newObj = GameObject.Instantiate(baseButtonPrefab);
@@ -49,15 +56,15 @@ public class AlchemyMenuController : MonoBehaviour
                 newObj.GetComponentInChildren<Button>().onClick.AddListener(delegate { SetSelectedBase(alchemyBase); });
             }
 
-            for(int i = 0; i < numIngredients; i++)
+            for (int i = 0; i < numIngredients; i++)
             {
                 GameObject newObj = GameObject.Instantiate(selectedIngredientButton);
                 newObj.transform.parent = selectedPanel.transform;
                 currentButtons.Add(newObj);
             }
-
             PopulateApplicableInventory();
-        } catch(NullReferenceException e)
+        }
+        catch (NullReferenceException e)
         {
             Debug.Log("closing");
         }
@@ -72,10 +79,10 @@ public class AlchemyMenuController : MonoBehaviour
 
     private void PopulateApplicableInventory()
     {
-        foreach(Transform trans in player.Bag.transform)
+        foreach (Transform trans in player.Bag.transform)
         {
             IItem item = trans.gameObject.GetComponent<IItem>();
-            foreach(int tag in item.Tags)
+            foreach (int tag in item.Tags)
             {
                 if (tag == 3)
                 {
@@ -91,5 +98,50 @@ public class AlchemyMenuController : MonoBehaviour
     private void SetSelectedIngredient(IItem ingredient)
     {
         Debug.Log("setting " + ingredient.Name);
+    }
+
+    private void updateHeat(int value)
+    {
+        if (!valueUpdated)
+        {
+            valueUpdated = true;
+            heatInputField.text = value.ToString();
+        }
+    }
+
+    private void updateHeat(string text)
+    {
+        if (!valueUpdated)
+        {
+            int value;
+            try
+            {
+                value = int.Parse(text);
+            }
+            catch (FormatException e)
+            {
+                value = 300;
+                heatInputField.text = "";
+            }
+            valueUpdated = true;
+            heatSlider.value = value;
+        }
+
+    }
+
+    private void clearPanels()
+    {
+        foreach (Transform child in basePanel.transform)
+        {
+            Destroy(child.gameObject);
+        }
+        foreach (Transform child in inventoryPanel.transform)
+        {
+            Destroy(child.gameObject);
+        }
+        foreach (Transform child in selectedPanel.transform)
+        {
+            Destroy(child.gameObject);
+        }
     }
 }
